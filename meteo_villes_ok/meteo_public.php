4f204f082229edb72448133ab72626e07180a75b
Damien Broggini <damienbroggini@hotmail.fr>
2021-01-04 11:26:09 +0100

    meteo

---
+++ meteo_villes_ok/meteo_public.php
@@ -0,0 +1,51 @@
+<?php
+# Traitement formulaire
+if (!empty($_POST['insee'])) {
+    $inseeSelected = ($_POST['insee']);
+    $url = "https://api.meteo-concept.com/api/forecast/daily/0?token=a9b814eaf38dd3c21fe83f56700730b9511b1692a27759760e5806dc70caedf8&insee=" . $inseeSelected;
+    $data = file_get_contents($url);
+    if ($data) {
+        $decoded = json_decode($data);
+        $city = $decoded->city;
+        $forecast = $decoded->forecast;
+        $meteoDisplayed = "Aujourd'hui à <strong>{$city->name}</strong>, on prévoit <strong>{$forecast->rr10}mm</strong> 
+        (pas plus de {$forecast->rr1}mm en tous cas) de précipitations. Météo du jour : {$forecast->weather}";
+    }
+}
+?>
+
+
+<form action="" method="POST">
+<label for="city">choisir une ville</label>
+<select name="insee" id="city">
+    <option value="">--choisir une ville--</option>
+<?php
+$cities = [
+    '35238' => 'Rennes',
+    '83050' => 'Draguignan',
+    '83137' => 'Toulon',
+    '13214' => 'Marseille 14e',
+    '06088' => 'Nice',
+    '06069' => 'Grasse',
+    '83023' => 'Brignoles'
+];
+foreach ($cities as $insee => $city) {
+    if ($insee == $inseeSelected) {
+        echo "<option value =\"$insee\" selected>$city</option>";
+    } else {
+        echo "<option value =\"$insee\">$city</option>";
+    }
+}
+?>
+</select>
+<input type="submit" name="submit" value="Générer"></input>
+</form>
+
+<?php
+if (!empty($meteoDisplayed)) {
+    echo "<p>$meteoDisplayed</p>";
+}
+?>
+
+    <!-- ////////////////////CODE FORECAST////////////////////////////// -->
+
